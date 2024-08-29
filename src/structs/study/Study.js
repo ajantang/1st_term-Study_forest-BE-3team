0d672ae604d3e2f879c09f7c6aa02c1587b383f4
DESKTOP-EHH0K4D\SnowRang <ajantang@naver.com>
2024-08-29 12:07:25 +0900

    feat : 스터디 정보 수정에 배경 수정이 안되도록 되어 있는 부분 수정
    기존에 검증하는 함수에 배경 정보가 없어서 수정 시도시 400 에러
    배경 수정 간으하도록 검증 코드 추가

--- src/structs/study/Study.js
+++ src/structs/study/Study.js
@@ -12,10 +12,13 @@ export const CreateStudy = s.object({
   password: s.size(s.string(), 8, 24),
 });
 
-export const PatchStudy = s.partial(s.object({
-  ...CreateStudyBody,
-  point: s.min(s.integer(), 0)
-}));
+export const PatchStudy = s.partial(
+  s.object({
+    ...CreateStudyBody,
+    background: s.size(s.string(), 0, 50),
+    point: s.min(s.integer(), 0),
+  })
+);
 
 export const Password = s.object({
   password: s.size(s.string(), 8, 24),
