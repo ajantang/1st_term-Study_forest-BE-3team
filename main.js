8539244dade17e45eeb8d97beac37ccfc2f36e97
root <root@DESKTOP-UE7169K>
2024-08-30 19:16:43 +0900

    f

--- main.js
+++ main.js
@@ -317,7 +317,7 @@ app.get(
         ).milliseconds;
         const diff = Math.floor(diffInDays / (1000 * 60 * 60 * 24)) + 6;
 
-        return [successDay, timeZoneMilisec];
+        return [successDay, timeZoneMilisec, UTCTime];
       });
 
       return {
